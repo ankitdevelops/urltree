67d50c08cc93a3a75828064472ebc6654150d532
Ankit <heyankitandrel@gmail.com>
2021-05-04 17:14:59 +0530

    first commit

--- urltree/settings.py
+++ urltree/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'staticfiles')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'staticfiles')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
