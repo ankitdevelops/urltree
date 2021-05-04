80fade76671e170c57b79fba645eb14e805b3090
Ankit <heyankitandrel@gmail.com>
2021-05-04 17:05:12 +0530

    first commit

--- urltree/settings.py
+++ urltree/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'staticfiles')
 ]
