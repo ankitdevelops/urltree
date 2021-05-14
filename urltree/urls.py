67b0a8bc1418e0c6a63edd42d44cb43e648d16da
Ankit <heyankitandrel@gmail.com>
2021-05-14 18:50:09 +0530

    few UI Changes

--- urltree/urls.py
+++ urltree/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('links.urls')),
     path('accounts/', include('accounts.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
